3fcdabf6542462738cfb2c0eb891e4a1c5bebc3a
Hapopa <82116969+Hapopa@users.noreply.github.com>
2022-12-18 11:06:01 +0200

    edit accoout setting

---
+++ lib/student/personal setting/account_seeting.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AccountSetting extends StatelessWidget {
+  String title;
+  String routeName;
+  AccountSetting(this.title, this.routeName);
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: InkWell(
+          onTap: (){
+            Navigator.pushNamed(context, routeName);
+          },
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey[600],
+                ),
+              ),
+              Icon(Icons.arrow_forward_ios,color: Colors.grey),
+            ],
+          ),
+        ),
+      );
+  }
+}
